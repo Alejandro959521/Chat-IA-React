af307765fe4e0b280476a16a7b288fb7fdca4c7a
Miguel Ojeda <cabrerasm081@gmail.com>
2026-08-20 19:55:33 -0400

    change in title

--- src/presentation/layouts/DashboardLayout.tsx
+++ src/presentation/layouts/DashboardLayout.tsx
@@ -6,7 +6,7 @@ export const DashboardLayout = () => {
         <main className="flex flex-row mt-7">
             <nav className="hidden sm:flex flex-col ml-5 w-96 min-h-[calc(100vh-3.0rem)] bg-white bg-opacity-10 p-5 rounded-3xl">
                 <h1 className="font-bold text-lg lg:text-3xl bg-linear-to-br from-white via-white/50 bg-clip-text text-transparent">
-                    ReactGPT<span className="text-indigo-500">.</span>
+                    Chat IA<span className="text-indigo-500">.</span>
                 </h1>
                 <span className="text-xl">Bienvenido</span>
 
